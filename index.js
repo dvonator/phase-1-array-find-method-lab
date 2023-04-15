ff1dbb97a4d14894a39134ec90abf7b1642c2de4
David <davidvo1992@gmail.com>
2023-04-15 13:42:35 -0500

    submit lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(array) {
+  const winObj = array.find(newResult); //.find method takes a function
+  return winObj ? winObj.year : undefined; //? checks if this first result exists else returns undefined
+}
+
+function newResult(passResults) {
+  return passResults.result === "W"; //this is the true or false for the find method
+}
